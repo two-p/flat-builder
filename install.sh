762e4229f85ab11b630b4741e34637866ac11b3c
Didier Page <didier.page@heig-vd.ch>
2020-03-10 21:09:14 +0100

    <feat><installation> add php-xml

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 add-apt-repository ppa:rwky/graphicsmagick
 apt update
-apt install dnsutils curl php-cli php-mbstring git unzip nodejs npm imagemagick software-properties-common graphicsmagick rsync -y
+apt install dnsutils curl php-cli php-mbstring php-xml git unzip nodejs npm imagemagick software-properties-common graphicsmagick rsync -y
 cd ~
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
